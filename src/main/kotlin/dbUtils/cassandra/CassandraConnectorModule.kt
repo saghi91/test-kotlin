a2dd3576087877cfbff3ccfee45b055940cebf2f
Saghi Ghorbani <saghi.ghorbani@metronom.com>
2019-09-27 19:06:12 +0200

    add singleton annotation to db connector

--- src/main/kotlin/dbUtils/cassandra/CassandraConnectorModule.kt
+++ src/main/kotlin/dbUtils/cassandra/CassandraConnectorModule.kt
@@ -1,8 +1,10 @@
 package dbUtils.cassandra
 
 import com.google.inject.AbstractModule
+import com.google.inject.Singleton
 import dbUtils.DBConnector
 
+@Singleton
 class CassandraConnectorModule : AbstractModule() {
     override fun configure() {
         bind(DBConnector::class.java).toProvider(CassandraConnectorProvider::class.java)
